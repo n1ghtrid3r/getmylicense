bb1e4b57e27c05b3758f656e0207f473cbc6c754
Jas Bindra <jasbindra00@hotmail.com>
2022-05-17 15:28:59 +0100

    update

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ var MAXIMUM_TEST_DATE = new Date(MAXIMUM_TEST_DATE_STRING);
 var MAXIMUM_TEST_DATE_SECONDS = MAXIMUM_TEST_DATE.getTime() / 1000;
 
 
+FIND_ALL_TESTS_REGARDLESS_OF_DATE = true;
+
 found_centres = document.querySelectorAll(".test-centre-details-link");
 
 for(const node of found_centres){
@@ -12,7 +14,9 @@ for(const node of found_centres){
         CENTRE_AVAILIBILITY_DATE_STRING = node.querySelector("h5").innerHTML.split(" ").at(-1).split("/").reverse().join("-");
         CENTRE_AVAILIBILITY_TEST_DATE = new Date(CENTRE_AVAILIBILITY_DATE_STRING);
         CENTRE_AVAILABILITY_SECONDS = CENTRE_AVAILIBILITY_TEST_DATE.getTime() / 1000;
-        if(CENTRE_AVAILABILITY_SECONDS <= MAXIMUM_TEST_DATE_SECONDS){
+
+
+        if(FIND_ALL_TESTS_REGARDLESS_OF_DATE || CENTRE_AVAILABILITY_SECONDS <= MAXIMUM_TEST_DATE_SECONDS){
             alert("FOUND TEST AT " + centre_title + " ON " + CENTRE_AVAILIBILITY_DATE_STRING);
         }
     }
