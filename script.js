90f9bce76880445fa0d3eb98e081aa6e2486d294
n1ghtrid3r <60359484+n1ghtrid3r@users.noreply.github.com>
2022-05-17 15:19:44 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-lst = document.querySelectorAll("a");
-var filter = Array.prototype.filter;
+
+MAXIMUM_TEST_DATE_STRING = "2022-07-01";
+var MAXIMUM_TEST_DATE = new Date(MAXIMUM_TEST_DATE_STRING);
+var MAXIMUM_TEST_DATE_SECONDS = MAXIMUM_TEST_DATE.getTime() / 1000;
 
 
-filtered = filter.call(lst, function(node){
-    return node.innerHTML.includes("available tests around");
-});
+found_centres = document.querySelectorAll(".test-centre-details-link");
 
-for(const element of filtered){
-    alert(element.innerHTML);
+for(const node of found_centres){
+    if(node.innerHTML.includes("available tests around")){
+        centre_title = node.querySelector("h4").innerHTML;
+        CENTRE_AVAILIBILITY_DATE_STRING = node.querySelector("h5").innerHTML.split(" ").at(-1).split("/").reverse().join("-");
+        CENTRE_AVAILIBILITY_TEST_DATE = new Date(CENTRE_AVAILIBILITY_DATE_STRING);
+        CENTRE_AVAILABILITY_SECONDS = CENTRE_AVAILIBILITY_TEST_DATE.getTime() / 1000;
+        if(CENTRE_AVAILABILITY_SECONDS <= MAXIMUM_TEST_DATE_SECONDS){
+            alert(CENTRE_AVAILIBILITY_DATE_STRING)
+        }
+    }
 }
-return filtered;
